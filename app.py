19e07fd40e69efe05433b0549e19e0d951b9923e
juanmanueldupi <juanmadupi@gmail.com>
2022-04-29 21:04:12 +0200

    Primeras funciones en el app.py

---
+++ app.py
@@ -0,0 +1,29 @@
+from flask import Flask, render_template, request, redirect, abort
+import os
+import json
+
+def LeerLibreria():
+    try:
+        f=open("MSX.json")
+        datos = json.load(f)
+        f.close
+        return datos
+    except:
+        print("Error al leer el fichero")
+
+libros=LeerLibreria()
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/juegos')
+def juegos():
+    return render_template('juegos.html')
+
+
+
+port=os.environ["PORT"]
+app.run("0.0.0.0",int(port),debug=True)
